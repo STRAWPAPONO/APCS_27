dc83d2355ead6220d41337beeb115d0f8852f29a
STRAWPAPONO <branden.chung.2011@gmail.com>
2026-09-03 19:35:30 +0000

    update

--- 000_Labs/004_String_class/base_code/starter.java
+++ 000_Labs/004_String_class/base_code/starter.java
@@ -9,6 +9,12 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		String part1 = new String("Happy Birthday");
+		String part2 = new String(", to you.");
+		String name = new String("Justin");
+		System.out.println(part1+part2);
+		System.out.println(part1+part2);
+		System.out.println(part1+" dear "+name+",");
+		System.out.println(part1+part2);
 	}
 }
